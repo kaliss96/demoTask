87da05f964c6201ec870f2884a6984782c1e155b
kaliss96 <aguirretellez96@gmail.com>
2020-12-17 22:06:13 -0600

    Add files via upload
    update files

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //constantes token de api y url 
-define("API_TOKEN","55676080f1464a1c4410687f1d4c25401ce1c0f322df7f3e591a00");
+define("API_TOKEN","55646084f447541559457a6d4a5625401ce3c0fa2cda78365e1b06");
 define("URL_API","https://api.tookanapp.com/v2/");
 define("SITE_NAME","demoTask");
 $defaultController="TaskControllers";
@@ -21,21 +21,18 @@ $nsegments=explode("/",$uri);
 
 if(isset($nsegments[1]) && $nsegments[1]!="")
 {
-
-$segmentos=_set_uri_string($uri);
+	$segmentos=_set_uri_string($uri);
 }
 else
-$segmentos[1]=$defaultController;
+	$segmentos[1]=$defaultController;
 
 if(!in_array($segmentos[1],array("js","css")))
 {
-	 
-require("controller/".$segmentos[1].".php");
-$_controlador=strtolower($segmentos[1]);
-$$_controlador=new $segmentos[1]();
+	require("controller/".$segmentos[1].".php");
+	$_controlador=strtolower($segmentos[1]);
+	$$_controlador=new $segmentos[1]();
 
-$method=(isset($segmentos[2])?$segmentos[2]:"index");
+	$method=(isset($segmentos[2])?$segmentos[2]:"index");
 
-${strtolower($defaultController)}->{$method}();
- 
+	${strtolower($defaultController)}->{$method}();
 }
